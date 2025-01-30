0a3e7a393e0762b0df4ba26e5e2741f906c90eed
wahyudiaditya <wahyudiaditya007@gmail.com>
2025-01-30 21:31:39 +0700

    Feat: - add apollo package - setup mongodb config - setup env - setup app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+import "dotenv/config";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const server = new ApolloServer({
+  typeDefs: [],
+  resolvers: [],
+  introspection: true,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: process.env.PORT || 3005 },
+  context: async ({ req, res }) => {
+    const authN = async () => {
+      const getToken = req.headers.authorization;
+
+      if (!getToken) {
+        throw new Error("Unauthorized");
+      }
+      const [type, token] = getToken.split(" ");
+
+      const isValidToken = verifyToken(token);
+
+      if (!isValidToken) {
+        throw new Error("Unauthorized");
+      }
+      // const user = await User.findById(isValidToken.id);
+
+      // return user;
+    };
+
+    return {
+      authentication: () => authN(),
+    };
+  },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
